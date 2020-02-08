823acf4ab9f51a2b8110d04f952670f694cace14
Vikas <vikas.pal.8445@gmail.com>
2020-02-08 23:23:58 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,port,conversion_multiple)
+values(1001,'USD','INR',0,65);
+insert into exchange_value(id,currency_from,currency_to,port,conversion_multiple)
+values(1002,'EUR','INR',0,75);
+insert into exchange_value(id,currency_from,currency_to,port,conversion_multiple)
+values(1003,'AUD','INR',0,25);
